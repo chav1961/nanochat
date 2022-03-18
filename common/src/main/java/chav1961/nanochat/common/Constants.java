ad3574047e3ed3caf188c76a06632ab76cbbb34d
chav1961 <chav1961@mail.ru>
2022-03-18 09:16:25 +0500

    Daily commit

---
+++ common/src/main/java/chav1961/nanochat/common/Constants.java
@@ -0,0 +1,13 @@
+package chav1961.nanochat.common;
+
+public class Constants {
+	public static final String	PROP_GENERAL = "general";
+	public static final String	PROP_GENERAL_ID = "uuid";
+	public static final String	PROP_GENERAL_NAME = "name";
+	public static final String	PROP_GENERAL_DEFAULT_LANG = "defaultLang";
+	public static final String	PROP_GENERAL_TRAY_LANG_EN = "trayLangEn";
+
+	public static final String	PROP_ANARCH = "anarch";
+	public static final String	PROP_ANARCH_SUBNETS = "subnets";
+	public static final String	PROP_ANARCH_DISTRICT = "district";
+}
