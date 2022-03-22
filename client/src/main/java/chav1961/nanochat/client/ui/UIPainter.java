7f4cd4aab8b6bd47b1b224e6e76b81c22f4db7ce
chav1961 <chav1961@mail.ru>
2022-03-22 17:12:25 +0500

    Daily commit

---
+++ client/src/main/java/chav1961/nanochat/client/ui/UIPainter.java
@@ -0,0 +1,16 @@
+package chav1961.nanochat.client.ui;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class UIPainter implements Closeable {
+	public UIPainter() {
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
